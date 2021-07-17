47c54650129cae2fa6fd5202fa15ebdc6d980685
Geduardi <eduardilgen@gmail.com>
2021-07-17 17:53:20 +0200

    Заработала авторизация.

--- app/Services/SocialService.php
+++ app/Services/SocialService.php
@@ -15,19 +15,11 @@ class SocialService
         $email = $user->getEmail();
         $authUser = User::where('email', $email)->first();
 //        $authUser = User::all()->where('email', $email)->first();
-
         if ($authUser){
-//            \Auth::login($authUser);
+            \Auth::login($authUser);
             $authUser->name = $user->getName();
             $authUser->avatar = $user->getAvatar();
-            dump($authUser->name);
-            dump($authUser->avatar);
-            dump($authUser);
-//            dd($authUser->save);
-            dd($authUser->save);
-            return $authUser->save;
-
-
+            return $authUser->save();
         }
 
         return false;
